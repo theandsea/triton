6da7906ec1c4f93401c708d24dc0e6b2c21c282f
theandsea <the_and_sea@mail.163.com>
2023-05-19 22:26:07 -0700

    jsp update6

--- tritonlink/FTM.java
+++ tritonlink/FTM.java
@@ -5,7 +5,7 @@ import java.sql.Statement;
 import java.sql.Driver;
 // import com.mysql.jdbc.Driver;
 
-public class FTMx {
+public class FTMxxxx {
     public static void main(String[] args) {
         //try{
             //Class.forName("com.mysql.cj.jdbc.Driver"); // this is in mysql
