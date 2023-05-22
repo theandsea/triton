3c4b3a127d84b384e06c4be07af8e03df3d0c97c
theandsea <45749967+theandsea@users.noreply.github.com>
2023-05-21 20:03:02 -0700

    Update FTM.java

--- tritonlink/FTM.java
+++ tritonlink/FTM.java
@@ -5,7 +5,7 @@ import java.sql.Statement;
 import java.sql.Driver;
 // import com.mysql.jdbc.Driver;
 
-public class FTMxxxx {
+public class FTM {
     public static void main(String[] args) {
         //try{
             //Class.forName("com.mysql.cj.jdbc.Driver"); // this is in mysql
